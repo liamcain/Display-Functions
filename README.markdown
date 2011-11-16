eb9f7e23e8993b2f03bdd8c8a37ff9850c1cc37c
Liam Cain <contactliam529@gmail.com>
2011-11-16 17:09:25 -0500

    Now uses autocomplete instead of Goto Anything

--- README.markdown
+++ README.markdown
@@ -2,4 +2,4 @@
 
 ### Plugin that opens "goto anything," filled it with the class of whatever object you have selected. (Only works with Java at the moment)
 
-#####Todo: copy the function name, escape the overlap panel, and paste function name in the window
+####Todo: copy the function name, escape the overlap panel, and paste function name in the window
--- functioncopier.py
+++ functioncopier.py
@@ -1,19 +1,36 @@
-import sublime, sublime_plugin
-import re 
+import sublime
+import sublime_plugin
+import re
+
+completions = []
 
 class CopyFunctionCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		sel = self.view.sel()[0]
-		word = self.view.word(sel)
-		string = self.view.substr(word).strip()		
-		regions = self.view.find_all('(?<![\\w])'+re.escape(string)+'\\b')
+    def run(self, edit):
+        sel = self.view.sel()[0]
+        word = self.view.word(sel)
+        string = self.view.substr(word).strip()
+        regions = self.view.find_all('(?<![\\w])' + re.escape(string) + '\\b')
 
-		for r in regions:
-			sel_begin = self.view.word(r).begin()
-			prev_end = sel_begin - 2
-			prev_word = self.view.substr(self.view.word(prev_end)) + "@"
+        self.view.insert(edit, sel.end(), ".")
 
-			if "storage.type" in self.view.scope_name(r.begin() - 2):
-				self.view.window().run_command("show_overlay", {"overlay": "goto", "text": prev_word})
-				return
-		self.view.window().run_command("show_overlay", {"overlay": "goto", "text": "@"})
+        for r in regions:
+            sel_begin = self.view.word(r).begin()
+            prev_end = sel_begin - 2
+            prev_word = self.view.substr(self.view.word(prev_end))
+
+            if "storage.type" in self.view.scope_name(r.begin() - 2):
+                for method in get_methods(prev_word):
+                    completions.append(method)
+        self.view.run_command('auto_complete', {'disable_auto_insert': True})
+    
+    def get_methods(prev_word):
+        
+
+class FillAutoComplete(sublime_plugin.EventListener):
+    def on_query_completions(self, view, prefix, locations):
+        id = view.id()
+        if id in completions:
+            _completions = completions[id]
+            del completions[id]
+            return _completions
+        return []

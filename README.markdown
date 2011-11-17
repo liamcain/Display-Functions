f563b27a20ca7423132aa93546633d652dc4a653
Liam Cain <contactliam529@gmail.com>
2011-11-16 22:11:08 -0500

    fixed autocomplete

--- README.markdown
+++ README.markdown
@@ -1,5 +1,6 @@
-# Function Copier
+## Function Copier
 
-###Finds an objects type, adds the methods of the object into the autocomplete box, and actives autocomplete
+####Finds an objects type, adds the methods of the object into the autocomplete box, and actives autocomplete
 
-###Problems: I can't seem to add anything into the autocomplete (something worked once, but it was a fluke and I can't reproduce); I don't know how to search through a different file (see comments for more info)
+
+####Got the autocomplete working.  Next is loading in the methods...
--- functioncopier.py
+++ functioncopier.py
@@ -21,28 +21,19 @@ class CopyFunctionCommand(sublime_plugin.TextCommand):
             print self.view.substr(prev_word)
             if "storage.type" in self.view.scope_name(prev_word.begin()):
                 print "Class: " + self.view.substr(prev_word)
-                for method in self.add_functions(prev_word):
-                    #Here is the methods to completions.
-
-                    print "adds the methods to Completions here"
+                self.add_functions(prev_word)
 
                 break
-            self.view.run_command('auto_complete', {'disable_auto_insert': True})
+        
+        self.view.run_command('auto_complete', {'disable_auto_insert': True})
         return
 
     #This function will take a classname and return a list of all the class methods
     #Will use find_by_selector(selector)
     def add_functions(self, classname):
-        methods = []
-        
-        return methods
+        completions.append("")
     
 
 class FillAutoComplete(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
-        id = view.id()
-        if id in completions:
-            _completions = completions[id]
-            del completions[id]
-            return _completions
-        return []
+        return [(x, x) for x in completions]

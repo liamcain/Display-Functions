74e4713243d96696c146cf0c98bdeb7a61b5cd81
Liam Cain <contactliam529@gmail.com>
2011-11-16 20:12:50 -0500

    changed for all regions

--- functioncopier.py
+++ functioncopier.py
@@ -5,26 +5,38 @@ import re
 completions = []
 
 class CopyFunctionCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         sel = self.view.sel()[0]
-        word = self.view.word(sel)
+        word = self.view.word(sel.end() - 1)
         string = self.view.substr(word).strip()
         regions = self.view.find_all('(?<![\\w])' + re.escape(string) + '\\b')
 
-        self.view.insert(edit, sel.end(), ".")
+        #self.view.insert(edit, sel.end(), ".") # this is for when the plugin is eventually mapped to "."
 
         for r in regions:
-            sel_begin = self.view.word(r).begin()
-            prev_end = sel_begin - 2
-            prev_word = self.view.substr(self.view.word(prev_end))
+            prev_end = r.begin() - 2
+            prev_word = self.view.word(prev_end)
 
-            if "storage.type" in self.view.scope_name(r.begin() - 2):
-                for method in get_methods(prev_word):
-                    completions.append(method)
-        self.view.run_command('auto_complete', {'disable_auto_insert': True})
-    
-    def get_methods(prev_word):
+            print self.view.substr(prev_word)
+            if "storage.type" in self.view.scope_name(prev_word.begin()):
+                print "Class: " + self.view.substr(prev_word)
+                for method in self.add_functions(prev_word):
+                    #Here is the methods to completions.
+
+                    print "adds the methods to Completions here"
+
+                break
+            self.view.run_command('auto_complete', {'disable_auto_insert': True})
+        return
+
+    #This function will take a classname and return a list of all the class methods
+    #Will use find_by_selector(selector)
+    def add_functions(self, classname):
+        methods = []
         
+        return methods
+    
 
 class FillAutoComplete(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
